dd3e7d247712196f60d3c8daf8c6d15333799d0a
geckoil <73975841+geckoil@users.noreply.github.com>
2022-08-15 18:14:29 +0700

    nun

---
+++ fizzbuzz.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+#define bruh        &&
+#define fr          ;
+#define bobux       int
+#define chief       main
+#define finna       =
+#define be          {
+#define tho         }
+#define kinda       ==
+#define aint        !=
+#define no          !
+#define deadass     --
+#define grindin     for
+#define yeet        return
+#define ongod       ++
+#define realtalk    printf
+#define ngl         if
+#define cap         else
+#define lil         <=
+#define widda       ,
+#define bussin      *
+#define vibecheck   %
+#define bout        (
+#define aight       )
+#define nun         void
+
+bobux chief bout nun aight
+be
+    grindin bout
+        bobux num finna 1 fr
+        num lil 100 fr
+        ongod num aight
+    be
+        bobux mod3 finna num vibecheck 3 fr
+        bobux mod5 finna num vibecheck 5 fr
+        ngl bout mod3 aint 0 bruh mod5 aint 0 aight
+            realtalk bout "%d" widda num aight fr
+        cap
+        be
+            ngl bout mod3 kinda 0 aight
+                realtalk bout "fizz" aight fr
+            ngl bout mod5 kinda 0 aight
+                realtalk bout "buzz" aight fr
+        tho
+        realtalk bout "\n" aight fr
+    tho
+    yeet 0 fr
+tho
