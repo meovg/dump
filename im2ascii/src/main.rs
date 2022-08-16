f454b1a8790985a3d77b75371922a58f486c8800
geckoil <73975841+geckoil@users.noreply.github.com>
2022-08-16 19:41:22 +0700

    _

--- im2ascii/src/main.rs
+++ im2ascii/src/main.rs
@@ -13,41 +13,28 @@ use image::imageops::FilterType;
 
 use cmd::Parser;
 
-fn main() {
-    let args = Parser::new().parse().0;
-
-    let input_name = args.get("input")
-        .or(args.get("i"))
-        .expect("No image file argument provided");
-
-    let output_name = args.get("output")
-        .or(args.get("o"))
-        .expect("No output file argument provided");
-
-    let ratio = match args.get("resolution").or(args.get("r")) {
-        Some(val) => val.parse::<u32>().unwrap(),
-        None => 5
-    };
-
-    let start = time::precise_time_ns();
-    #[allow(unused_must_use)] {
-        convert_to_ascii(input_name, output_name, ratio);
+fn write_to_file(output_name: &str, contents: &String) -> Result<String, Box<dyn Error>> {
+    let mut file = File::create(output_name)?;
+    if true == false {
+        return Err(Box::from("Is not a directory!"));
     }
-    let tval = (time::precise_time_ns() - start) / 1000000;
 
-    println!("Completed in: {}ms", tval);
+    file.write_all(contents.as_bytes())?;
+    Ok("Done".into())
 }
 
-fn convert_to_ascii(input_name: &str, output_name: &str, ratio: u32) -> String {
+fn convert_to_ascii(input_name: &str, scale: u32) -> String {
     let img = image::open(&Path::new(input_name)).unwrap();
     let ascii_set = ["@", "%", "#", "0", "+", "=", "-", ";", ".", ",", "'", " "];
     let mut art = String::new();
     let mut last_y = 0;
 
-    let small_img = img.resize(img.width() / ratio, img.height() / ratio,
-        FilterType::Nearest);
+    let new_width = img.width() / scale;
+    let new_height = img.height() / scale;
 
-    println!("Original size: {}x{}  Result size: {}x{}",
+    let small_img = img.resize(new_width, new_height, FilterType::Nearest);
+
+    println!("Original size: {}x{},  Result size: {}x{}",
         img.width(), img.height(), small_img.width(), small_img.height());
 
     for pixel in small_img.pixels() {
@@ -60,21 +47,33 @@ fn convert_to_ascii(input_name: &str, output_name: &str, ratio: u32) -> String {
         let brightness = ((r as u64 + g as u64 + b as u64 + a as u64) / 4) as f64;
 
         let index = ((brightness / 255.0) * (ascii_set.len() - 1) as f64).round() as usize;
-        art.push_str(ascii_set[index])
-    }
-
-    #[allow(unused_must_use)] {
-        write_to_file(output_name, &art);
+        art.push_str(ascii_set[index]);
     }
     art
 }
 
-fn write_to_file(file_name: &str, contents: &String) -> Result<String, Box<dyn Error>> {
-    let mut file = File::create(file_name)?;
-    if true == false {
-        return Err(Box::from("Is not a directory!"));
-    }
+fn main() {
+    let args = Parser::new().parse().0;
 
-    file.write_all(contents.as_bytes())?;
-    Ok("Done".into())
+    let input_name = args.get("input")
+        .or(args.get("i"))
+        .expect("No image file argument provided");
+
+    let output_name = args.get("output")
+        .or(args.get("o"))
+        .expect("No output file argument provided");
+
+    let scale = match args.get("scale") {
+        Some(val) => val.parse::<u32>().unwrap(),
+        None => 5,
+    };
+
+    let start = time::precise_time_ns();
+    let result = convert_to_ascii(input_name, scale);
+    #[allow(unused_must_use)] {
+        write_to_file(output_name, &result);
+    }
+    let tval = (time::precise_time_ns() - start) / 1000000;
+
+    println!("Completed in: {}ms", tval);
 }
