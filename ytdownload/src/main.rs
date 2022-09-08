83258a27844af53acb7b558987abd618ce5a7e3d
geckoil <73975841+geckoil@users.noreply.github.com>
2022-09-08 22:04:53 +0700

    ytdownload: remove panic in main function

--- ytdownload/src/main.rs
+++ ytdownload/src/main.rs
@@ -18,28 +18,26 @@ struct Flags {
     audiofmt: String,
 }
 
-fn main() -> anyhow::Result<()> {
+fn main() {
     let args = Flags::parse();
 
     let video_id = match fetcher::get_video_id(&args.url) {
         Err(err) => {
-            println!("Cannot parse the YouTube video ID: {:?}", err);
-            return Ok(()); // i'm scared of panics
+            println!("Cannot parse the YouTube video ID: [{:?}]", err);
+            return; // i'm scared of panics
         }
         Ok(id) => id,
     };
 
     let video_info = match fetcher::get_video_info(&video_id) {
         Err(err) => {
-            println!(
-"Cannot retrieve video info for download. Possibly the video no longer exists, is restricted\n\
-or YouTube API has updated beyond the current method of gathering info\n {:?}", err);
-            return Ok(());
+            println!("Cannot request YouTube for video info: [{:?}]", err);
+            return;
         }
         Ok(info) => info,
     };
 
-    let title = fetcher::get_video_title(&video_info)?;
+    let title = fetcher::get_video_title(&video_info).unwrap();
 
     // alter video file name so it does not contain illegal characters in windows directory
     // ref: https://stackoverflow.com/questions/1976007/
@@ -50,16 +48,22 @@ or YouTube API has updated beyond the current method of gathering info\n {:?}", 
 
     let video_link = match fetcher::get_video_download_url(&video_info) {
         Err(err) => {
-            println!("Cannot find a suitable video stream to download: {:?}", err);
-            return Ok(());
+            println!("Cannot find a suitable video stream to download: [{:?}]", err);
+            return;
+        }
+        Ok("") => {
+            println!(
+                "Cannot get the direct video URL.\nThe video may be restricted, deleted, \
+                or the YouTube API has been updated beyond this method of getting URL.");
+            return;
         }
         Ok(link) => link,
     };
 
     let video_pathbuf = match download::download(video_link, &video_filename) {
         Err(err) => {
-            println!("Problem occured. Video download is halted: {:?}", err);
-            return Ok(());
+            println!("Problem occured. Video download is halted: [{:?}]", err);
+            return;
         }
         Ok(vid_pb) => vid_pb,
     };
@@ -75,10 +79,8 @@ or YouTube API has updated beyond the current method of gathering info\n {:?}", 
         let audio_file = Path::new(&audio_filename);
 
         cmd::save_audio(video_file, audio_file);
-        fs::remove_file(&video_file)?;
+        fs::remove_file(&video_file).unwrap();
     }
 
     println!("Download complete");
-
-    Ok(())
 }
