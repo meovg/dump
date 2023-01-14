33d04f80f5b59633d325296dca7fae2a65a7d647
geckoil <73975841+geckoil@users.noreply.github.com>
2023-01-14 15:59:01 +0700

    lc3: fix code style

--- lc3/assembler/main.c
+++ lc3/assembler/main.c
@@ -3,24 +3,21 @@
 #include <string.h>
 #include <stdio.h>
 
-/* ===== token ===== */
-
 #define MAX_TOKEN_NUM 7
 #define MAX_TOKEN_LEN 1025
 #define MAX_LEN 7175
 
 typedef struct {
     uint16_t size;
-    char *raw;
+    char *str;
 } Token;
 
 typedef struct {
     uint8_t size;
-    Token **raw;
-} TokenBuf;
+    Token **arr;
+} TokenList;
 
-char get_escape_value(char c)
-{
+char get_escape_value(char c) {
     static const char escape_chars[] = {'\'', '\"', '\?', '\\', 'a', 'b', 'f', 'n', 'r', 't', 'v'};
     static const char escape_vals[] = {0x27, 0x22, 0x3F, 0x5C, 0x07, 0x08, 0x0C, 0x0A, 0x0D, 0x09, 0x0B};
 
@@ -30,15 +27,13 @@ char get_escape_value(char c)
     return c;
 }
 
-void token_copy(Token *dst, const Token *src)
-{
+void token_copy(Token *dst, const Token *src) {
     dst->size = src->size;
-    dst->raw = malloc((dst->size + 1) * sizeof(char));
-    memcpy(dst->raw, src->raw, (dst->size + 1) * sizeof(char));
+    dst->str = malloc((dst->size + 1) * sizeof(char));
+    memcpy(dst->str, src->str, (dst->size + 1) * sizeof(char));
 }
 
-Token *token_from_string(const char *line, uint16_t *index_ptr)
-{
+Token *token_from_string(const char *line, uint16_t *index_ptr) {
     uint16_t i = *index_ptr;
     uint16_t j = 0;
     char tmp[MAX_TOKEN_LEN];
@@ -59,18 +54,17 @@ Token *token_from_string(const char *line, uint16_t *index_ptr)
 
     Token *t = malloc(sizeof *t);
     t->size = j + 1;
-    t->raw = malloc((t->size + 1) * sizeof(char));
+    t->str = malloc((t->size + 1) * sizeof(char));
     for (uint16_t k = 0; k < j; k++) {
-        t->raw[k] = tmp[k];
+        t->str[k] = tmp[k];
     }
-    t->raw[j] = '\0';
+    t->str[j] = '\0';
 
     *index_ptr = i;
     return t;
 }
 
-int8_t is_eow(char c)
-{
+int8_t is_eow(char c) {
     int8_t eow = 0;
     static const char eowchars[] = {0, 13, '\t', '\n', ' ', ',', ';'};
 
@@ -80,8 +74,7 @@ int8_t is_eow(char c)
     return eow;
 }
 
-Token *token_from_word(const char *line, uint16_t *index_ptr)
-{
+Token *token_from_word(const char *line, uint16_t *index_ptr) {
     uint8_t i = *index_ptr;
     uint8_t j = 0;
     char tmp[MAX_TOKEN_LEN];
@@ -94,18 +87,17 @@ Token *token_from_word(const char *line, uint16_t *index_ptr)
 
     Token *t = malloc(sizeof *t);
     t->size = j + 1;
-    t->raw = malloc((t->size + 1) * sizeof(char));
+    t->str = malloc((t->size + 1) * sizeof(char));
     for (uint8_t k = 0; k < j; k++) {
-        t->raw[k] = tmp[k];
+        t->str[k] = tmp[k];
     }
-    t->raw[j] = '\0';
+    t->str[j] = '\0';
 
     *index_ptr = i - 1;
     return t;
 }
 
-int8_t is_eol(char c)
-{
+int8_t is_eol(char c) {
     int8_t eol = 0;
     static const char eolchars[] = {0, 10, 13, 59};
 
@@ -116,13 +108,12 @@ int8_t is_eol(char c)
     return eol;
 }
 
-TokenBuf *tokenize(const char *line)
-{
-    TokenBuf *buf = malloc(sizeof *buf);
-    buf->size = 0;
-    buf->raw = malloc(sizeof(Token *));
+TokenList *tokenize(const char *line) {
+    TokenList *tl = malloc(sizeof *tl);
+    tl->size = 0;
+    tl->arr = malloc(sizeof(Token *));
 
-    uint8_t new_token = 0;
+    int8_t need_token = 0;
     int8_t is_sep = 0;
     int8_t is_quote = 0;
     int8_t prev_is_sep = 1;
@@ -133,146 +124,132 @@ TokenBuf *tokenize(const char *line)
         
         if (is_quote) {
             i++;
-            buf->raw[buf->size] = token_from_string(line, &i);
-            buf->size++;
-            new_token = 1;
+            tl->arr[tl->size] = token_from_string(line, &i);
+            tl->size++;
+            need_token = 1;
         } else if (prev_is_sep && !is_sep) {
-            buf->raw[buf->size] = token_from_word(line, &i);
-            buf->size++;
+            tl->arr[tl->size] = token_from_word(line, &i);
+            tl->size++;
             is_sep = 1;
-            new_token = 1;
+            need_token = 1;
         }
 
-        if (new_token) {
-            buf->raw = realloc(buf->raw, (buf->size + 1) * sizeof(Token *));
-            new_token = 0;
+        if (need_token) {
+            tl->arr = realloc(tl->arr, (tl->size + 1) * sizeof(Token *));
+            need_token = 0;
         }
-
         prev_is_sep = is_sep;
     }
 
-    return buf;
+    return tl;
 }
 
-void token_free(Token *t)
-{
+void token_free(Token *t) {
     if (t) {
-        if (t->raw) free(t->raw);
+        if (t->str) free(t->str);
         free(t);
     }
 }
 
-void token_buffer_free(TokenBuf *buf)
-{
-    if (!buf) return;
-    if (buf->raw) {
-        for (uint8_t i = 0; i < buf->size; i++) {
-            if (buf->raw[i]->size) {
-                token_free(buf->raw[i]);
+void tokenlist_free(TokenList *tl) {
+    if (!tl) return;
+    if (tl->arr) {
+        for (uint8_t i = 0; i < tl->size; i++) {
+            if (tl->arr[i]->size) {
+                token_free(tl->arr[i]);
             }
         }
-        free(buf->raw);
+        free(tl->arr);
     }
-    free(buf);
+    free(tl);
 }
 
-/* ===== lines ===== */
-
 typedef struct {
-    TokenBuf *buf;
-    uint32_t ln;
+    TokenList *toks;
+    uint32_t lnum;
 } Line;
 
 typedef struct {
     uint32_t size;
     uint32_t cap;
-    Line **raw;
-} LineArr;
+    Line **arr;
+} LineList;
 
-#define LINEARR_DEFAULT_SIZE 200
+#define LINELIST_DEFAULT_SIZE 200
 
-LineArr *linearr_new(void)
-{
-    LineArr *arr = malloc(sizeof *arr);
-    arr->cap = LINEARR_DEFAULT_SIZE;
-    arr->size = 0;
-    arr->raw = malloc(arr->cap * sizeof(Line *));
-    return arr;
+LineList *linelist_new(void) {
+    LineList *la = malloc(sizeof *la);
+    la->cap = LINELIST_DEFAULT_SIZE;
+    la->size = 0;
+    la->arr = malloc(la->cap * sizeof(Line *));
+    return la;
 }
 
-void linearr_set(LineArr *arr, TokenBuf *buf, uint32_t ln)
-{
-    if (arr->size == arr->cap) {
-        arr->cap *= 2;
-        arr->raw = realloc(arr->raw, arr->cap * sizeof(Line *));
+void linelist_add(LineList *ll, TokenList *tl, uint32_t lnum) {
+    if (ll->size == ll->cap) {
+        ll->cap *= 2;
+        ll->arr = realloc(ll->arr, ll->cap * sizeof(Line *));
     }
-    arr->raw[arr->size] = malloc(sizeof(Line *));
-    arr->raw[arr->size]->buf = buf;
-    arr->raw[arr->size]->ln = ln;
-    arr->size++;
+    ll->arr[ll->size] = malloc(sizeof(Line));
+    ll->arr[ll->size]->toks = tl;
+    ll->arr[ll->size]->lnum = lnum;
+    ll->size++;
 }
 
-Line *linearr_get(LineArr *arr, uint32_t ind)
-{
-    if (ind >= arr->size) return NULL;
-    return arr->raw[ind];
+Line *linelist_get(LineList *ll, uint32_t idx) {
+    if (idx >= ll->size) return NULL;
+    return ll->arr[idx];
 }
 
-void linearr_free(LineArr *arr)
-{
-    if (!arr) return;
-    for (uint32_t i = 0; i < arr->size; i++) {
-        token_buffer_free(arr->raw[i]->buf);
-        free(arr->raw[i]);
+void linelist_free(LineList *ll) {
+    if (!ll) return;
+    for (uint32_t i = 0; i < ll->size; i++) {
+        tokenlist_free(ll->arr[i]->toks);
+        free(ll->arr[i]);
     }
-    free(arr->raw);
-    free(arr);
+    free(ll->arr);
+    free(ll);
 }
-
-/* ===== file buffer ===== */
 
 typedef struct {
     uint16_t *addrs;
     uint16_t *instrs;
-    uint32_t *lns;
+    uint32_t *lnums;
     uint16_t size;
     uint16_t cap;
-} FileBuf;
+} OutBuffer;
 
-#define FILEBUF_DEFAULT_SIZE 128
+#define OUTBUFFER_DEFAULT_SIZE 128
 
-FileBuf *filebuf_new(void)
-{
-    FileBuf *buf = malloc(sizeof *buf);
-    buf->size = 0;
-    buf->cap = FILEBUF_DEFAULT_SIZE;
-    buf->addrs = malloc(buf->cap * sizeof(uint16_t));
-    buf->instrs = malloc(buf->cap * sizeof(uint16_t));
-    buf->lns = malloc(buf->cap * sizeof(uint32_t));
-    return buf;
+OutBuffer *outbuffer_new(void) {
+    OutBuffer *ob = malloc(sizeof *ob);
+    ob->size = 0;
+    ob->cap = OUTBUFFER_DEFAULT_SIZE;
+    ob->addrs = malloc(ob->cap * sizeof(uint16_t));
+    ob->instrs = malloc(ob->cap * sizeof(uint16_t));
+    ob->lnums = malloc(ob->cap * sizeof(uint32_t));
+    return ob;
 }
 
-void filebuf_add(FileBuf *buf, uint16_t addr, uint16_t instr, uint32_t ln)
-{
-    if (buf->size == buf->cap) {
-        buf->cap *= 2;
-        buf->addrs = realloc(buf->addrs, buf->cap * sizeof(uint16_t));
-        buf->instrs = realloc(buf->instrs, buf->cap * sizeof(uint16_t));
-        buf->lns = realloc(buf->lns, buf->cap * sizeof(uint32_t));
+void outbuffer_add(OutBuffer *ob, uint16_t addr, uint16_t instr, uint32_t lnum) {
+    if (ob->size == ob->cap) {
+        ob->cap *= 2;
+        ob->addrs = realloc(ob->addrs, ob->cap * sizeof(uint16_t));
+        ob->instrs = realloc(ob->instrs, ob->cap * sizeof(uint16_t));
+        ob->lnums = realloc(ob->lnums, ob->cap * sizeof(uint32_t));
     }
-    buf->addrs[buf->size] = addr;
-    buf->instrs[buf->size] = instr;
-    buf->lns[buf->size] = ln;
-    buf->size++;
+    ob->addrs[ob->size] = addr;
+    ob->instrs[ob->size] = instr;
+    ob->lnums[ob->size] = lnum;
+    ob->size++;
 }
 
-void filebuf_free(FileBuf *buf)
-{
-    if (!buf) return;
-    if (buf->addrs) free(buf->addrs);
-    if (buf->instrs) free(buf->instrs);
-    if (buf->lns) free(buf->lns);
-    free(buf);
+void outbuffer_free(OutBuffer *ob) {
+    if (!ob) return;
+    if (ob->addrs) free(ob->addrs);
+    if (ob->instrs) free(ob->instrs);
+    if (ob->lnums) free(ob->lnums);
+    free(ob);
 }
 
 /* ===== files ===== */
@@ -284,23 +261,20 @@ typedef struct {
     FILE *lst;
 } FileList;
 
-int8_t check_extension(char *filename, const char *ext)
-{
+int8_t check_extension(char *filename, const char *ext) {
     char *s = strrchr(filename, '.');
     if (!s) return 0;
     return !strcmp(s, ext);
 }
 
-void replace_extension(char *filename, const char *ext)
-{
+void replace_extension(char *filename, const char *ext) {
     char *s = strrchr(filename, '.');
     for (int i = 0; s[i] != '\0'; i++) {
         s[i] = ext[i];
     }
 }
 
-uint8_t filelist_open(FileList *fl, char *filename)
-{
+uint8_t filelist_open(FileList *fl, char *filename) {
     uint8_t err = 0;
 
     fl->src = fopen(filename, "r");
@@ -308,35 +282,30 @@ uint8_t filelist_open(FileList *fl, char *filename)
         printf("Unable to open assembly file: %s!\n", filename);
         err++;
     }
-
     replace_extension(filename, ".sym");
     fl->sym = fopen(filename, "w");
     if (fl->sym == NULL) {
         printf("Unable to create symbol table file: %s!\n", filename);
         err++;
     }
-
     replace_extension(filename, ".obj");
     fl->obj = fopen(filename, "w");
     if (fl->obj == NULL) {
         printf("Unable to create object file: %s!\n", filename);
         err++;
     }
-
     replace_extension(filename, ".lst");
     fl->lst = fopen(filename, "w");
     if (fl->lst == NULL) {
         printf("Unable to create list file: %s!\n", filename);
         err++;
     }
-
     replace_extension(filename, ".asm");
 
     return err;
 }
 
-void filelist_close(FileList *fl)
-{
+void filelist_close(FileList *fl) {
     if (!fl) return;
     if (fl->src) fclose(fl->src);
     if (fl->sym) fclose(fl->sym);
@@ -344,8 +313,7 @@ void filelist_close(FileList *fl)
     if (fl->lst) fclose(fl->lst);
 }
 
-uint8_t clean_output(char *filename)
-{
+uint8_t clean_output(char *filename) {
     uint8_t err = 0;
     static const char *exts[] = {".sym", ".obj", ".lst"};
 
@@ -363,46 +331,42 @@ uint8_t clean_output(char *filename)
     return err;
 }
 
-void write_obj(FileBuf *buf, FILE *obj)
-{
-    uint16_t byte_cnt = buf->size;
+void write_obj(OutBuffer *ob, FILE *obj) {
+    uint16_t byte_cnt = ob->size;
     uint8_t tmp[byte_cnt];
 
-    for (uint16_t i = 0; i < buf->size; i++) {
-        tmp[i * 2] = buf->instrs[i] >> 8; // high byte
-        tmp[i * 2 + 1] = buf->instrs[i] & 0xff; // low byte
+    for (uint16_t i = 0; i < ob->size; i++) {
+        tmp[i * 2] = ob->instrs[i] >> 8; // high byte
+        tmp[i * 2 + 1] = ob->instrs[i] & 0xff; // low byte
     }
 
     fwrite(tmp, sizeof(uint8_t), byte_cnt, obj);
 }
 
-void write_lst(FileBuf *buf, FILE *lst, FILE *src)
-{
-    static const char *hextobin[16] = {
+void write_lst(OutBuffer *ob, FILE *lst, FILE *src) {
+    static const char *bin[16] = {
         "0000", "0001", "0010", "0011", "0100", "0101", "0110", "0111",
         "1000", "1001", "1010", "1011", "1100", "1101", "1110", "1111",
     };
 
+    #define binseq(x) \
+        bin[x >> 12], \
+        bin[(x >> 8) & 0xf], \
+        bin[(x >> 4) & 0xf], \
+        bin[x & 0xf]
+
     char line[MAX_LEN + 1];
-    uint16_t idx = 1;
-    uint16_t lim = buf->size;
+    uint16_t idx = 0; // (doubt)
+    uint16_t lim = ob->size;
 
     fseek(src, 0, SEEK_SET); // find beginning of .asm file
     fprintf(lst, "  Addr  |  Hex  |       Bin        | Line |  Source\n");
 
     for (uint32_t ln = 1; fgets(line, MAX_LEN + 1, src); ln++) {
-        if (idx != lim && buf->lns[idx] == ln) {
-            uint16_t instr = buf->instrs[idx];
-            uint16_t addr = buf->addrs[idx];
-
-            fprintf(lst, " x%04X  | x%04X | %s%s%s%s ",
-                addr,
-                instr,
-                hextobin[instr >> 12],
-                hextobin[(instr >> 8) & 0xf],
-                hextobin[(instr >> 4) & 0xf],
-                hextobin[instr & 0xf]
-            );
+        if (idx != lim && ob->lnums[idx] == ln) {
+            uint16_t instr = ob->instrs[idx];
+            uint16_t addr = ob->addrs[idx];
+            fprintf(lst, " x%04X  | x%04X | %s%s%s%s ", addr, instr, binseq(instr));
             idx++;
         } else {
             fprintf(lst, "        |       |                  ");
@@ -411,16 +375,9 @@ void write_lst(FileBuf *buf, FILE *lst, FILE *src)
         fprintf(lst, "| %4d | %s", ln, line);
 
         // multiple instructions case
-        while (idx != lim && buf->lns[idx] == ln) {
-            uint16_t instr = buf->instrs[idx];
-
-            fprintf(lst, "        | x%04X | %s%s%s%s |      |\n",
-                instr,
-                hextobin[instr >> 12],
-                hextobin[(instr >> 8) & 0xf],
-                hextobin[(instr >> 4) & 0xf],
-                hextobin[instr & 0xf]
-            );
+        while (idx != lim && ob->lnums[idx] == ln) {
+            uint16_t instr = ob->instrs[idx];
+            fprintf(lst, "        | x%04X | %s%s%s%s |      |\n", instr, binseq(instr));
             idx++;
         }
     }
